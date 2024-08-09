82c62e4e7b4690fc74ff5308a58c27624bc00931
mr_sd_jack_003 <barson0habra@gmail.com>
2024-08-09 12:46:34 +0530

    remove some html code and pasted it to the index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,18 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import {html, raw} from 'hono/html'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.html(
+    html`<!doctype html>
+      <script src="https://unpkg.com/htmx.org@2.0.1"></script>
+      <button hx-post='/clicked' hx-swap="outerHTML">Click Me</button>
+
+    `,
+  );
+});
 
 app.post("/clicked", (c) => {
   return c.text("Hello Hono!!, you clicked this button");
